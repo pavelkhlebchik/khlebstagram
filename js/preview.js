05edc9f423404df82069748dd3fcb5e0598b1c84
Pavel_khlebchick <pavelkhlebchik@gmail.com>
2021-05-31 22:21:15 +0300

    Добавляет счетчик для масштабирования загружаемой фото

--- js/preview.js
+++ js/preview.js
@@ -1,9 +1,26 @@
 'use strict';
 (function () {
   const imgUpload = document.querySelector(`.img-upload__overlay`);
-  const scaleControlSmaller = imgUpload.querySelector(`.scale__controll--smaller`);
-  const scaleControlBigger = imgUpload.querySelector(`.scale__controll--bigger`);
-  const scaleControlValue = imgUpload.querySelector(`.scale__controll--value`);
+  const scaleControlSmaller = imgUpload.querySelector(`.scale__control--smaller`);
+  const scaleControlBigger = imgUpload.querySelector(`.scale__control--bigger`);
+  const scaleControlValue = imgUpload.querySelector(`.scale__control--value`);
+  let minScale = 0;
+  console.log(minScale);
+  let maxScale = 100;
+  // вынести к утилитам и удалить обработчик событий
+  scaleControlBigger.addEventListener(`click`, function () {
+    if (minScale < 100) {
+      minScale++;
+      scaleControlValue.value = minScale + `%`;
+    }
+  });
+
+  scaleControlSmaller.addEventListener(`click`, function () {
+    if (maxScale > 0) {
+      minScale--;
+      scaleControlValue.value = minScale + `%`;
+    }
+  });
 
   const effectLevelPin = imgUpload.querySelector(`.effect-level__pin`);
   const effectLevelDepth = imgUpload.querySelector(`.effect-level__depth`);
